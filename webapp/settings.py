5d20f9d2e5ef293c701115a5317ac235c81f8061
Alex Banks <alex.banx98@gmail.com>
2020-05-28 17:52:46 +0100

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -24,7 +24,7 @@ try:
     with open('secret_key.txt') as f:
         SECRET_KEY = f.read().strip()
 except FileNotFoundError:
-    SECRET_KEY = os.environ['SECRET KEY']
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
